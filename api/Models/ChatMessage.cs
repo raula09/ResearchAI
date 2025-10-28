2af4accff34adea29d04381a0dd7ec6c6d1eeec6
raula09 <davitoniani0602@gmail.com>
2025-10-28 23:54:11 +0400

    fixes

--- api/Models/ChatMessage.cs
+++ api/Models/ChatMessage.cs
@@ -6,6 +6,6 @@ namespace ResearchCopilot.Api.Models
     public class ChatMessage
     {
         [BsonId] public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
-        
+            public string UserId { get; set; } = "";
     }
 }
